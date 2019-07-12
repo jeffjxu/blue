9766b41901337becf75f7e80d9000f0cd44d81b3
Jeff Xu <jeff@jeffs-mbp.austin.ibm.com>
2019-07-12 11:54:08 -0500

    add readme

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('dotenv').config({silent: true});
+
+var server = require('./app');
+var port = process.env.PORT || 3001;
+
+server.listen(port, function() {
+    // eslint-disable-next-line
+    console.log('Server running on port: %d', port);
+});
